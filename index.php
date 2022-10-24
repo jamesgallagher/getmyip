169de5a561f89f27f5b586483ed9989f0fa80e31
James Gallagher <james@tailormade.co.za>
2022-10-25 09:46:48 +1100

    Changed provider to allow more api requests per day

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?php  
-    function getIPAddress() {  
-     if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
-                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+    function getIPAddress() 
+    {  
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+            $ip = $_SERVER['HTTP_CLIENT_IP'];  
         }  
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
-     }  
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+        }  
     else{  
-             $ip = $_SERVER['REMOTE_ADDR'];  
-     }  
-     return $ip;  
+        $ip = $_SERVER['REMOTE_ADDR'];  
+    }  
+    
+    return $ip;  
 }  
+
 $ip = getIPAddress();  
 
-// Free API allowing 1000 calls per day. 
-$url = 'https://api.db-ip.com/v2/free/' . trim($ip);
+// Switch API provider to provider that allows 42 reqs per minute 
+$url = 'http://ip-api.com/json/' . trim($ip);
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
